0ff0f6feaf2045d46b0113c44521818ba68250d5
Yumin Gui <guiyumin@protonmail.com>
2025-05-19 17:00:42 -0700

    feat: setup arco design

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 import reactLogo from "@/assets/react.svg";
 import wxtLogo from "/wxt.svg";
 import "./App.css";
-
+import { Button } from "@arco-design/web-react";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button type="primary">点击</Button>
+    </>
+  );
 }
 
 export default App;
